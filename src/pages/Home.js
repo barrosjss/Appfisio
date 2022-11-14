57a75dca4cb9dc250227f644236c821616696041
Jesus Barros <jesus.reinaldo.barros@gmail.com>
2022-11-14 02:44:49 -0500

    Update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,175 +2,178 @@ import React from 'react'
 
 function home() {
   return (
-    <div className="container" style={{marginBottom: 10,}}>
-      <div className="card first-section" style={{ padding: 10, }}>
-        <h5 class="card-title">Deficiencias funcionales corporales</h5>
-        <p>Seleccione los enunciados con los que tenga dificultad:</p>
+    <div className="container">
+      <div className="p-5 mb-4 bg-light rounded-3 border">
+        <div className="container-fluid py-5">
+          <h2 className="display-5 fw-bold">Deficiencias funcionales corporales</h2>
+          <p className="col-md-8 fs-4">Seleccione los enunciados con los que tenga dificultad:</p>
 
-        <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
-          <label class="form-check-label" for="flexCheckDefault">
-            Movilidad de varias articulaciones
-          </label>
-        </div>
-        <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
-          <label class="form-check-label" for="flexCheckDefault">
-            Funciones de la presion arterial
-          </label>
-        </div>
-        <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
-          <label class="form-check-label" for="flexCheckDefault">
-            Funciones relacionadas con la fuerza muscular
-          </label>
-        </div>
-        <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
-          <label class="form-check-label" for="flexCheckDefault">
-            Fuerza de musculos aislados y grupos musculares
-          </label>
-        </div>
-        <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
-          <label class="form-check-label" for="flexCheckDefault">
-            Funciones relacionadas con la resistencia
-          </label>
-        </div>
-        <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
-          <label class="form-check-label" for="flexCheckDefault">
-            Resistencia de grupos musculares
-          </label>
-        </div>
-        <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
-          <label class="form-check-label" for="flexCheckDefault">
-            Ninguna de las anteriores
-          </label>
+          <div className="form-check">
+            <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
+            <label className="form-check-label" for="flexCheckDefault">
+              Movilidad de varias articulaciones
+            </label>
+          </div>
+          <div className="form-check">
+            <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
+            <label className="form-check-label" for="flexCheckDefault">
+              Funciones de la presion arterial
+            </label>
+          </div>
+          <div className="form-check">
+            <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
+            <label className="form-check-label" for="flexCheckDefault">
+              Funciones relacionadas con la fuerza muscular
+            </label>
+          </div>
+          <div className="form-check">
+            <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
+            <label className="form-check-label" for="flexCheckDefault">
+              Fuerza de musculos aislados y grupos musculares
+            </label>
+          </div>
+          <div className="form-check">
+            <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
+            <label className="form-check-label" for="flexCheckDefault">
+              Funciones relacionadas con la resistencia
+            </label>
+          </div>
+          <div className="form-check">
+            <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
+            <label className="form-check-label" for="flexCheckDefault">
+              Resistencia de grupos musculares
+            </label>
+          </div>
+          <div className="form-check">
+            <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
+            <label className="form-check-label" for="flexCheckDefault">
+              Ninguna de las anteriores
+            </label>
+          </div>
         </div>
       </div>
-      <div className="card second-section" style={{ padding: 10, }}>
-        <h5 class="card-title">Deficiencias estructurales corporales</h5>
-        <p>Seleccione los enunciados con los que tenga una deficiencia estructural</p>
 
-        <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
-          <label class="form-check-label" for="flexCheckDefault">
+      <div className="p-5 mb-4 bg-light rounded-3 border">
+        <h2 className="display-5 fw-bold">Deficiencias estructurales corporales</h2>
+        <p className="col-md-8 fs-4">Seleccione los enunciados con los que tenga una deficiencia estructural</p>
+
+        <div className="form-check">
+          <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
+          <label className="form-check-label" for="flexCheckDefault">
             Huesos de la region del hombro
           </label>
         </div>
-        <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
-          <label class="form-check-label" for="flexCheckDefault">
+        <div className="form-check">
+          <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
+          <label className="form-check-label" for="flexCheckDefault">
             Articulaciones de la region del hombro
           </label>
         </div>
-        <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
-          <label class="form-check-label" for="flexCheckDefault">
+        <div className="form-check">
+          <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
+          <label className="form-check-label" for="flexCheckDefault">
             Musculos de la region del hombro
           </label>
         </div>
-        <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
-          <label class="form-check-label" for="flexCheckDefault">
+        <div className="form-check">
+          <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
+          <label className="form-check-label" for="flexCheckDefault">
             Ligamentos y fascias de la region del hombro
           </label>
         </div>
-        <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
-          <label class="form-check-label" for="flexCheckDefault">
+        <div className="form-check">
+          <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
+          <label className="form-check-label" for="flexCheckDefault">
             Articulacion del codo
           </label>
         </div>
-        <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
-          <label class="form-check-label" for="flexCheckDefault">
+        <div className="form-check">
+          <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
+          <label className="form-check-label" for="flexCheckDefault">
             Ninguna de las anteriores
           </label>
         </div>
-
-        
-
       </div>
 
-      <div className="card third-section" style={{ padding: 10, }}>
-        <h5 class="card-title">Limitaciones en las actividades</h5>
-        <p>Seleccione que actividades no puede realizar con normalidad</p>
-        <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
-          <label class="form-check-label" for="flexCheckDefault">
+      <div className="p-5 mb-4 bg-light rounded-3 border">
+        <h2 className="display-5 fw-bold">Limitaciones en las actividades</h2>
+        <p className="col-md-8 fs-4">Seleccione que actividades no puede realizar con normalidad</p>
+
+        <div className="form-check">
+          <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
+          <label className="form-check-label" for="flexCheckDefault">
             Vestirse
           </label>
         </div>
-        <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
-          <label class="form-check-label" for="flexCheckDefault">
+        <div className="form-check">
+          <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
+          <label className="form-check-label" for="flexCheckDefault">
             Comer
           </label>
         </div>
-        <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
-          <label class="form-check-label" for="flexCheckDefault">
+        <div className="form-check">
+          <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
+          <label className="form-check-label" for="flexCheckDefault">
             Ponerse el calzado
           </label>
         </div>
-        <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
-          <label class="form-check-label" for="flexCheckDefault">
+        <div className="form-check">
+          <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
+          <label className="form-check-label" for="flexCheckDefault">
             Quitarse el calzado
           </label>
         </div>
-        <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
-          <label class="form-check-label" for="flexCheckDefault">
+        <div className="form-check">
+          <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
+          <label className="form-check-label" for="flexCheckDefault">
             Cepillarse los dientes
           </label>
         </div>
-        <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
-          <label class="form-check-label" for="flexCheckDefault">
+        <div className="form-check">
+          <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
+          <label className="form-check-label" for="flexCheckDefault">
             Peinarse
           </label>
         </div>
-        <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
-          <label class="form-check-label" for="flexCheckDefault">
+        <div className="form-check">
+          <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
+          <label className="form-check-label" for="flexCheckDefault">
             Higiene personal - procesos de excresion
           </label>
         </div>
-        <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
-          <label class="form-check-label" for="flexCheckDefault">
+        <div className="form-check">
+          <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
+          <label className="form-check-label" for="flexCheckDefault">
             Ninguna de las anteriores
           </label>
         </div>
       </div>
-      <div className="card third-section" style={{ padding: 10, }}>
-        <h5 class="card-title">Restricciones en la participacion</h5>
-        <p>Seleccione que actividades no puede realizar con normalidad</p>
-        <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
-          <label class="form-check-label" for="flexCheckDefault">
+
+      <div className="p-5 mb-4 bg-light rounded-3 border">
+        <h2 className="display-5 fw-bold">Restricciones en la participacion</h2>
+        <p className="col-md-8 fs-4">Seleccione que actividades no puede realizar con normalidad</p>
+
+        <div className="form-check">
+          <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
+          <label className="form-check-label" for="flexCheckDefault">
             Relaciones familiares
           </label>
         </div>
-        <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
-          <label class="form-check-label" for="flexCheckDefault">
+        <div className="form-check">
+          <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
+          <label className="form-check-label" for="flexCheckDefault">
             Mantener el trabajo
           </label>
         </div>
-        <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
-          <label class="form-check-label" for="flexCheckDefault">
+        <div className="form-check">
+          <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
+          <label className="form-check-label" for="flexCheckDefault">
             Tiempo libre y ocio
           </label>
         </div>
-        <div class="form-check">
-          <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
-          <label class="form-check-label" for="flexCheckDefault">
+        <div className="form-check">
+          <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"></input>
+          <label className="form-check-label" for="flexCheckDefault">
             Ninguna de las anteriores
           </label>
         </div>
